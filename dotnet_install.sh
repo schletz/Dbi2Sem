c4eb695b0763dc1511fc7281f5ce22e9538f135c
schletz <schletz@spengergasse.at>
2022-11-05 17:20:45 +0100

    Commit 05/11/2022

--- dotnet_install.sh
+++ dotnet_install.sh
@@ -1,4 +1,4 @@
-cd /home
+cd /tmp
 VERSION=$(curl -s https://dotnetcli.azureedge.net/dotnet/Sdk/6.0/latest.version)
 INSTALLFILE=dotnet-sdk-$VERSION-linux-x64.tar.gz
 
@@ -6,7 +6,7 @@ if [ ! -f "$INSTALLFILE" ]; then
     curl -s https://dotnetcli.azureedge.net/dotnet/Sdk/$VERSION/$INSTALLFILE > $INSTALLFILE
 fi
 echo Installiere .NET $VERSION
-mkdir -p /home/dotnet && tar zxf $INSTALLFILE -C /home/dotnet
-export DOTNET_ROOT=/home/dotnet
-export PATH=$PATH:/home/dotnet
+mkdir -p /tmp/dotnet && tar zxf $INSTALLFILE -C /tmp/dotnet
+export DOTNET_ROOT=/tmp/dotnet
+export PATH=$PATH:/tmp/dotnet
 export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
