df1cf7a3086981599e6cf52dc59eb064ceffaf26
schletz <schletz@spengergasse.at>
2022-11-06 08:56:39 +0100

    Commit 06/11/2022

--- start_dotnet.sh
+++ start_dotnet.sh
@@ -1,9 +1,15 @@
+#!/bin/bash
+# Date: November 2022
+# Author: Michael Schletz
+# Description: Installiert die .NET SDK in /tmp/dotnetx.xx.xx und lädt ein tar Archiv
+# von der übergebenen URL in /tmp/dotnetapp/(timestamp) und führt danach den Befehl mit
+# dotnet run aus. Bei einem oracle Container wird der Parameter oracle übergeben, sonst sqlserver
 if [ -d "/opt/oracle" ]; then DOWNLOADER="curl -s"; else DOWNLOADER="wget -q -O /dev/stdout"; fi
 
 VERSION=$($DOWNLOADER https://dotnetcli.azureedge.net/dotnet/Sdk/6.0/latest.version)
 INSTALLFILE=dotnet-sdk-$VERSION-linux-x64.tar.gz
 DOTNET_HOME=/tmp/dotnet$VERSION
-APP_DIR=/tmp/$(date +'%s')
+APP_DIR=/tmp/dotnetapp/$(date +'%s')
 
 if [ ! -d "$DOTNET_HOME" ]; then
     echo Installiere .NET $VERSION
